cbe0841c63ba4c7a798c1f40683b611e671e4450
anemeth <andras.akos.nemeth@prezi.com>
2014-11-07 10:48:32 +0100

    using regexp in filters instead of match

--- app/filter/filterByConfiguration.filter.js
+++ app/filter/filterByConfiguration.filter.js
@@ -2,11 +2,11 @@ angular.module("JenkinsDashboard")
 .filter("filterByConfiguration", function(Conf) {
 	return function(jobs, lastBuild) {
 		var filtered = [];
-		var filterString = Conf.val.filter.toLowerCase();
+		var re = new RegExp(Conf.val.filter.toLowerCase());
 
 		angular.forEach(jobs, function(job) {
 			//filter for names
-			if (job.name.toLowerCase().match(filterString) !== null) {
+			if (re.test(job.name.toLowerCase())) {
 					filtered.push(job);
 					return;
 			}
@@ -14,7 +14,7 @@ angular.module("JenkinsDashboard")
 			//filter for user name
 			var lastBuildJob = lastBuild[job.name];
 			if (lastBuildJob.culprits && lastBuildJob.culprits.length > 0 && lastBuildJob.culprits[0] && lastBuildJob.culprits[0].fullName) {
-				if (lastBuildJob.culprits[0].fullName.toLowerCase().match(filterString) !== null) {
+				if (re.test(lastBuildJob.culprits[0].fullName.toLowerCase())) {
 					filtered.push(job); 
 					return;
 				}
@@ -25,11 +25,11 @@ angular.module("JenkinsDashboard")
 				var actions = lastBuildJob.actions;
 				for (var l = actions.length; l--;) {
 					if (actions[l] && actions[l].causes) {
-						if (actions[l].causes[0].userName && actions[l].causes[0].userName.toLowerCase().match(filterString) !== null) {
+						if (actions[l].causes[0].userName && re.test(actions[l].causes[0].userName.toLowerCase())) {
 							filtered.push(job); 
 							return;
 						}
-						if (actions[l].causes[0].shortDescription && actions[l].causes[0].shortDescription.toLowerCase().match(filterString) !== null) {
+						if (actions[l].causes[0].shortDescription && re.test(actions[l].causes[0].shortDescription.toLowerCase())) {
 							filtered.push(job); 
 							return;
 						}
@@ -41,7 +41,7 @@ angular.module("JenkinsDashboard")
 			if (lastBuildJob.changeSet) {
 				var changeSet = lastBuildJob.changeSet;
 				if (changeSet.items && changeSet.items.length > 0 && changeSet.items[0].msg) {
-					if (changeSet.items[0].msg.toLowerCase().match(filterString)) {
+					if (re.test(changeSet.items[0].msg.toLowerCase())) {
 						filtered.push(job); 
 						return;
 					}
