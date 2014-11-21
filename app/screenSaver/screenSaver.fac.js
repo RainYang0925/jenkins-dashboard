9c3cc734a594ae3f19ef073d3df9c36d21a33649
Simone Fonda <simone.fonda@prezi.com>
2014-11-21 12:32:56 +0100

    Using real giphy key

--- app/screenSaver/screenSaver.fac.js
+++ app/screenSaver/screenSaver.fac.js
@@ -1,8 +1,7 @@
 angular.module('JenkinsDashboard')
 .factory('ScreenSaver', function($timeout, $rootScope, $resource, Conf) {
 
-	// TODO: this is the 'beta' key, request a proper one when ready to go live!
-	var GIPHY_API_KEY = "dc6zaTOxFJmzC",
+	var GIPHY_API_KEY = "12ss5TcLvRjUze",
 		randomGIF = $resource('http://api.giphy.com/v1/gifs/random', { api_key: GIPHY_API_KEY });
 
 	var isShown = false,
@@ -57,7 +56,6 @@ angular.module('JenkinsDashboard')
 	}
 
 	function changeScreenSaver() {
-		/*
 		randomGIF.get({ tag: Conf.val.topic }, function(res) {
 			currentScreenSaver = res.data.image_url;
 			for (var l = changeImageCbs.length; l--;) {
@@ -65,13 +63,6 @@ angular.module('JenkinsDashboard')
 			}
 			console.log("New screensaver: ", currentScreenSaver);
 		});
-		*/
-
-		currentScreenSaver = "http://media.giphy.com/media/TabwFck9vEt44/giphy.gif";
-		for (var l = changeImageCbs.length; l--;) {
-			changeImageCbs[l].call();
-		}
-		console.log("New screensaver: ", currentScreenSaver);
 
 		if (isShown) {
 			startRotationTimer();
