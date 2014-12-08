e3656954db323f3a532d0f656e2ff86542b76506
Simone Fonda <simone.fonda@prezi.com>
2014-12-08 18:04:54 +0100

    Not using forever on deploeyed app

--- scripts/run_jenkinsdashboard.sh
+++ scripts/run_jenkinsdashboard.sh
@@ -2,5 +2,5 @@
 
 # exec node /opt/prezi/jenkinsdashboard/current/node_modules/gulp/bin/gulp.js
 
-cd node /opt/prezi/jenkinsdashboard/current/
-exec node node_modules/forever/bin/forever start server/server.js
+cd /opt/prezi/jenkinsdashboard/current/
+exec node server/server.js
